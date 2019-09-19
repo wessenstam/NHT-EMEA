caa99d3efcb9d71a3f658c770295a4d21d819dc7
Willem Essenstam <wessenstam@gmail.com>
2019-09-19 20:36:13 +0200

    Revert "Update"
    This reverts commit 2154955854ce27bdad6cc0cc0cff62b6ad2fe664.

--- stage/scripts/get_mac_ipmi.sh
+++ stage/scripts/get_mac_ipmi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in 48 112 91 92 85 73 2 100
 do
-	echo "10.42.$i.33,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.36 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
-	echo "10.42.$i.34,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.35 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
-	echo "10.42.$i.35,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.34 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
-	echo "10.42.$i.36,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.33 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
+	echo "10.42.$i.33,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.36 raw 0x30 0x21 | tail -c 18 | sed 's/ /:/g')"
+	echo "10.42.$i.34,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.35 raw 0x30 0x21 | tail -c 18 | sed 's/ /:/g')"
+	echo "10.42.$i.35,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.34 raw 0x30 0x21 | tail -c 18 | sed 's/ /:/g')"
+	echo "10.42.$i.36,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.33 raw 0x30 0x21 | tail -c 18 | sed 's/ /:/g')"
 done
