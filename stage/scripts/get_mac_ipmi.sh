6babb121e052f735aa19d91f875dd26565270f92
Darren W <dawoo@nutanix.com>
2019-10-24 16:32:18 +0100

    Update get_mac_ipmi.sh

--- stage/scripts/get_mac_ipmi.sh
+++ stage/scripts/get_mac_ipmi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in 48 112 91 92 85 73 2 100
 do
-	echo "10.42.$i.33,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.36 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
-	echo "10.42.$i.34,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.35 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
-	echo "10.42.$i.35,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.34 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
-	echo "10.42.$i.36,$(ipmitool -U ADMIN -P ADMIN -H 10.42.$i.33 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
+	echo "10.42.$i.33,$(ipmitool -I lanplus -U ADMIN -P ADMIN -H 10.42.$i.36 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
+	echo "10.42.$i.34,$(ipmitool -I lanplus -U ADMIN -P ADMIN -H 10.42.$i.35 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
+	echo "10.42.$i.35,$(ipmitool -I lanplus -U ADMIN -P ADMIN -H 10.42.$i.34 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
+	echo "10.42.$i.36,$(ipmitool -I lanplus -U ADMIN -P ADMIN -H 10.42.$i.33 lan print1 | grep "MAC Address" | tr -s " " | cut -d " " -f4)"
 done
